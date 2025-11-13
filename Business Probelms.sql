7b9e5aec7e3c0f6e0855a7f331df1bbebc2e28d8
Oliver Navarro <olicern@gmail.com>
2025-11-13 10:08:32 -0600

    Create Business Probelms.sql

---
+++ Business Probelms.sql
@@ -0,0 +1,9 @@
+1.¿Cúantos participantes aplicaron la encuesta durante los años?
+2.¿De que paises provienen los encuestados?
+3.¿De que paises provienen los encuestados?
+4.¿Qué porcentaje de encuestados ha recibido tratamiento para su salud mental?
+5.¿Cuáles son los 5 países con mayor porcentaje de encuestados que reportan haber recibido tratamiento?
+6.¿Existe relación entre el tamaño de la empresa y la probabilidad de haber recibido tratamiento?
+7.¿Cuál es la realción entre el género y la probabilidad de haber recibido tratamiento?
+8.¿Las personas que trabajan en diferentes entornos de trabajo muestran diferencias en salud mental?
+9.¿Qué factores (país, género, tamaño de empresa, cultura laboral) parecen correlacionarse más con el tratamiento?
